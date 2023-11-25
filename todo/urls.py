4a6fdc2e06cd987ebdef4f8a0339077aa8aba97d
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-11-25 23:43:08 +0530

    add & viewed

--- todo/urls.py
+++ todo/urls.py
@@ -22,4 +22,5 @@ from todo_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home')
+
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -3,11 +3,13 @@ from .models import Task
 
 
 # Create your views here.
+
 def home(request):
+
     if request.method == 'POST':
         name = request.POST.get('name')
         priority = request.POST.get('priority')
         obj = Task(name=name, priority=priority)
         obj.save()
-
-    return render(request, 'home.html')
+    obj = Task.objects.all()
+    return render(request, 'home.html', {'obj': obj})
